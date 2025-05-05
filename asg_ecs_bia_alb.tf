80a2a4d59ecca6f853312f32d839c7c9631b19f7
nelsonholanda <nelsonholanda@hotmail.com>
2025-05-05 17:09:09 -0300

    AUTO_SCALING BIA

---
+++ asg_ecs_bia_alb.tf
@@ -0,0 +1,26 @@
+resource "aws_autoscaling_group" "ecs" {
+name_prefix          = "cluster-ecs-bia-asg-"
+  vpc_zone_identifier = [local.subnet_zona_a_public,local.subnet_zona_b_public,local.subnet_zona_c_public]
+    min_size             = 0
+    desired_capacity     = 1
+    max_size             = 3
+    health_check_type    = "EC2"
+    health_check_grace_period = 0
+    protect_from_scale_in = false
+    
+    launch_template {
+       id      = aws_launch_template.ecs_ec2.id
+        version = "$Latest"
+    }
+    tag {
+        key                 = "Name"
+        value               = "cluster-bia-tf"
+        propagate_at_launch = true
+    }
+    tag {
+        key                 = "AmazonECSManaged"
+        value               = ""
+        propagate_at_launch = true
+    }
+
+}
