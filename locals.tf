1e38361a95095fae4ce17691c3da0b1c5b4e19ff
nelsonholanda <nelsonholanda@hotmail.com>
2025-05-05 15:31:53 -0300

    update subnets project bia

--- locals.tf
+++ locals.tf
@@ -4,4 +4,6 @@ locals {
     subnet_zona_a="subnet-0e0bcd2e2a8adba2d"
     subnet_zona_b="subnet-0f9fcf269b88cda43"
     subnet_zona_c_public="subnet-04658c17fa2043b78"
+    subnet_zona_a_public="subnet-0f058be866dd52df5"
+    subnet_zona_b_public="subnet-06ab5283cc3d621d0"
 }
